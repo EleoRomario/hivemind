68a9893d3a2d3895e406e337273146201aafbf97
EleoRomario <eleo.romario@gmail.com>
2024-03-13 11:38:02 -0500

    Actualizar estilos de la barra lateral

--- src/components/modules/Sidebar/Sidebar.tsx
+++ src/components/modules/Sidebar/Sidebar.tsx
@@ -8,20 +8,21 @@ export default function Sidebar() {
   const { expanded, toggleExpanded } = useStore();
 
   return (
-    <aside
-      className={`border-bunker-600 h-screen border border-y-0 transition ${expanded ? 'w-64' : 'w-16'}`}
-    >
+    <aside className={`h-screen transition ${expanded ? 'w-64' : 'w-16'}`}>
       <nav className="flex h-full flex-col">
-        <div className="border-bunker-600 relative flex h-16 items-center justify-center border-b">
-          <LayersLogo />
+        <div className="relative flex h-16 items-center justify-center border-r border-bunker-950">
+          <div className="flex gap-2">
+            <LayersLogo />
+            {expanded && <span className="font-extrabold">HiveMind</span>}
+          </div>
           <button
-            className="bg-bunker-600 absolute -right-3 flex size-6 items-center justify-center rounded-lg text-white"
+            className="absolute -right-3 flex size-6 items-center justify-center rounded-lg bg-bunker-600 text-white"
             onClick={toggleExpanded}
           >
             <ArrowRight className={`${expanded && 'rotate-180'} transition`} />
           </button>
         </div>
-        <ul className="flex w-full flex-1 flex-col gap-6 py-5">
+        <ul className="relative flex w-full flex-1 flex-col gap-6 py-5">
           {sidebar.map(({ href, icon, label }, index) => (
             <SidebarItem key={index} href={href} icon={icon} label={label} />
           ))}
@@ -40,16 +41,16 @@ export function SidebarItem({
 
   return (
     <li className="group/link relative flex w-full items-center px-5 py-1">
-      <div className="group-hover/link:bg-primary absolute left-0 h-full w-1 rounded-ee  rounded-se transition"></div>
+      <div className="absolute left-0 h-full w-1 rounded-ee rounded-se  transition group-hover/link:bg-primary"></div>
       <Link
         href={href}
-        className="group-hover/link:text-primary text-bunker-300 flex w-full min-w-6 flex-row items-center gap-4 text-sm font-medium capitalize transition"
+        className="flex w-full min-w-6 flex-row items-center gap-4 text-sm font-medium capitalize text-bunker-300 transition group-hover/link:text-primary"
       >
         <Icon className="block size-6" />
         {expanded && <span>{label}</span>}
       </Link>
       {!expanded && (
-        <div className="text-primary bg-bunker-900 absolute left-full z-20 w-28 scale-0 truncate rounded-xl px-2 py-1 capitalize transition group-hover/link:scale-100">
+        <div className="absolute left-full z-20 w-28 scale-0 truncate rounded-xl bg-bunker-900 px-2 py-1 capitalize text-primary transition group-hover/link:scale-100">
           <span>{label}</span>
         </div>
       )}
