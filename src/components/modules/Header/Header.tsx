dc27cd2884f4152dcef1d450d27332e6548c3364
EleoRomario <eleo.romario@gmail.com>
2024-03-13 11:38:38 -0500

    Eliminar clase CSS innecesaria en el encabezado

--- src/components/modules/Header/Header.tsx
+++ src/components/modules/Header/Header.tsx
@@ -2,7 +2,7 @@ import { Search, User } from '@/components/modules/Header';
 
 export default function Header() {
   return (
-    <header className="border-bunker-600 flex h-16 flex-row items-center justify-between border-b px-10">
+    <header className="flex h-16 flex-row items-center justify-between  px-10">
       <Search />
       <User />
     </header>
